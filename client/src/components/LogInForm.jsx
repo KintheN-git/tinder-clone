0956c17f8bd864367a9a23c0f6b6e9868a5d83bb
Hasret Karci <83207900+KintheN-git@users.noreply.github.com>
2025-03-10 11:58:23 +0300

    auth page update

---
+++ client/src/components/LogInForm.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+const LogInForm = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  return (
+    <form className="space-y-6">
+      <div>
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Email address
+        </label>
+        <div className="mt-1">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            autoComplete="email"
+            required
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-pink-500 focus:border-pink-500 sm:text-sm"
+          />
+        </div>
+      </div>
+      <div>
+        <label
+          htmlFor="password"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Password
+        </label>
+        <div className="mt-1">
+          <input
+            id="password"
+            name="password"
+            type="password"
+            autoComplete="current-password"
+            required
+            value={formData.password}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-pink-500 focus:border-pink-500 sm:text-sm"
+          />
+        </div>
+      </div>
+      <button
+        type="submit"
+        className={`w-full flex justify-center py-2 px-4 border border-transparent 
+					rounded-md shadow-sm text-sm font-medium text-white ${
+            isLoading
+              ? "bg-pink-400 cursor-not-allowed"
+              : "bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+          }`}
+        disabled={isLoading}
+      >
+        {isLoading ? "Signing in..." : "Sign in"}
+      </button>
+    </form>
+  );
+};
+
+export default LogInForm;
